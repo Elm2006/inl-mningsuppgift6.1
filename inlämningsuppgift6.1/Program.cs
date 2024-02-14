5e07218b86b4a8130aa60db4674c3dea6ccbc7fb
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-14 10:34:19 +0100

    Add project files.

---
+++ inlämningsuppgift6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv i ett tal");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv i ett tal");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv i ett tal");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine("Summan av de tre talen blir "+Addera(a,b,c));
+    }
+    static int Addera(int a, int b, int c)
+    {
+        return a + b+ c;
+    }
+}
